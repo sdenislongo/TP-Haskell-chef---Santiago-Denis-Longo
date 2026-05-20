7febc8e4c022783fe7cdbb35047df3062eb9aa92
Santiago <santidenislongo@gmail.com>
2026-05-20 20:48:59 -0300

    saque los tests, predefinidos

--- src/Spec.hs
+++ src/Spec.hs
@@ -3,9 +3,8 @@ import PdePreludat
 import Library
 import Test.Hspec
 
-correrTests :: IO ()
-correrTests = hspec $ do
-  describe "Test de ejemplo" $ do
-    it "El pdepreludat se instaló correctamente" $ do
-      doble 1 `shouldBe` 2
+--correrTests :: IO ()
+--correrTests = hspec $ do
+  --describe "Test de ejemplo" $ do
+
 
